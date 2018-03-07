2ce8496b18e6663324b1704d29c0877df5b50783
Goode <66180988@qq.com>
2018-03-07 22:44:00 +0800

    练习python条件判断

---
+++ hello.py
@@ -0,0 +1,26 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# name = input('Please input your name:')
+# print('hello,', name + '.')
+# print('%2d-%02d' %(3, 1))
+# print('%6.2f' % 3.1415926)
+# s1 = 72
+# s2 = 85
+# r = (s2 - s1) / s1
+# print('小明成绩提升了%4.1f' % r)
+# age = 15
+# if age >= 18:
+	# print('your age is', age)
+	# print('adult')
+# else:
+	# print('your age is', age)
+	# print('teenager')
+
+
+age = 2
+if age >= 18:
+	print('adult')
+elif age >= 14:
+	print('teenager')
+else:
+	print('kid')
