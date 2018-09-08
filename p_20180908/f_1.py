22bef3c6e9eb982a4e6ffd0569a07ff9973c5f72
Goode <66180988@qq.com>
2018-09-09 00:29:14 +0800

    随便练习一下

---
+++ p_20180908/f_1.py
@@ -0,0 +1,10 @@
+import random
+# print(random.random[0,100])
+# help(random.random)
+l1 = [i for i in range(10)]
+print(l1)
+
+l2 = random.shuffle(l1)
+print(l1)
+
+print(random.randint(0, 100))
---
+++ p_20180908/f_2.py
@@ -0,0 +1,4 @@
+with open(r'test01.txt', 'r') as f:
+    l = list(f)
+    for line in l:
+        print(line)
